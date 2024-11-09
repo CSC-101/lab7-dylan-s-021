7bc0b0a106bedf5d93f7f108fea731a5d2f0a3e6
dylan-s-021 <dylanspencer021@gmail.com>
2024-11-09 12:51:11 -0800

    Completed command_line.py

--- command_line.py
+++ command_line.py
@@ -1,1 +1,16 @@
+import sys
+import convert
 
+# Task 3
+
+if __name__ == '__main__':
+    print(sys.argv)
+    num_list = []
+    num_list_sum = 0
+    for i in range(len(sys.argv)):
+        num = convert.str_to_float(sys.argv[i])
+        if num is not None:
+            num_list.append(num)
+    for j in range(len(num_list)):
+        num_list_sum = num_list_sum + num_list[j]
+    print(num_list_sum)
