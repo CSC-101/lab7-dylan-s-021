b7ec1aa1ca52f9a31c946e5d999852a426566fb7
dylan-s-021 <dylanspencer021@gmail.com>
2024-11-09 12:49:54 -0800

    Complete convert.py

--- convert.py
+++ convert.py
@@ -1,1 +1,10 @@
-
+# Task 1
+# convert a string to a float
+# input: a string
+# output: a float value if the given string represents a float value
+def str_to_float(string:str):
+    try:
+        value = float(string)
+        return value
+    except ValueError as e:
+        print(e)
